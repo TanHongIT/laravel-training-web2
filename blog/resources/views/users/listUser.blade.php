8e2bec9c5626401e7b021c545a9cd8fd762ef70b
tanhongit <phuongtan12357nguyen@gmail.com>
2021-01-06 10:57:33 +0700

    edit css

--- blog/resources/views/users/listUser.blade.php
+++ blog/resources/views/users/listUser.blade.php
@@ -4,42 +4,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
     <title>Document</title>
 </head>
 
 <body>
-    <h1>asdasd</h1>
-
-    <table>
-        <thead>
-            <th>
-                ID
-            </th>
-            <th>
-                Name
-            </th>
-            <th>
-                User Name
-            </th>
-            <th>
-                Email
-            </th>
-            <th>
-                Action
-            </th>
-        </thead>
-        <tbody>
-            <?php foreach ($users as $user) : ?>
-                <tr>
-                    <td><?php echo $user->id; ?></td>
-                    <td><?php echo $user->name; ?></td>
-                    <td><?php echo $user->username; ?></td>
-                    <td><?php echo $user->email; ?></td>
-                    <td>Action</td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+    <div class="container">
+        <div class="header" style="text-align: center;">
+            <h2>List User</h2>
+        </div>
+        <br>
+            <table class="table" id="grvListStudents">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($users as $user) :  ?>
+                        <tr scope="row">
+                            <td><?php echo $user->id; ?></td>
+                            <td><?php echo $user->name; ?></td>
+                            <td><?php echo $user->username; ?></td>
+                            <td><?php echo $user->email; ?></td>
+                        </tr>
+                    <?php
+                    endforeach; ?>
+                </tbody>
+            </table>
+    </div>
 </body>
 
 </html>
